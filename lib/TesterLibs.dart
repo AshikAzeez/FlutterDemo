f6307863db928e998207f2871b0dad5405e81d80
AshikAzeez <46971607+AshikAzeez@users.noreply.github.com>
2024-03-26 23:23:36 +0530

    Update TesterLibs.dart

--- lib/TesterLibs.dart
+++ lib/TesterLibs.dart
@@ -1,1 +1,11 @@
-int calculate() {  return 3 * 56;}String getName() {  return 'Ashik';}class LibClass {  String libName = 'SampleLibForFlutte';}
+int calculate() {
+  return 3 * 56;
+}
+
+String getName() {
+  return 'Ashik';
+}
+
+class LibClass {
+  String libName = 'SampleLibForFlutte';
+}
