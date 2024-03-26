f0cd134ffe564a1e8fd7082b5af42759490a5dcb
AshikAzeez <46971607+AshikAzeez@users.noreply.github.com>
2024-03-26 23:23:28 +0530

    Update welcome_home.dart

--- lib/ui/welcome_home.dart
+++ lib/ui/welcome_home.dart
@@ -1,1 +1,30 @@
-import 'package:flutter/material.dart';//FIXME:Material is used for adding material design to widget.Material is general Widgetclass Welcome extends StatelessWidget {  @override  Widget build(BuildContext context) {    return new Material(        color: Colors.greenAccent,        borderRadius: BorderRadius.circular(70.0),        //center is very generic container        child: new Center(          child: new Text(            "Hello Everyone",            textDirection: TextDirection.ltr,            softWrap: true,            style: new TextStyle(              inherit: true,              color: Colors.deepPurple,              fontSize: 70.0,              fontStyle: FontStyle.italic,              letterSpacing: 2.0,              wordSpacing: 1.0,            ),            textAlign: TextAlign.center,            maxLines: 3,          ),        ));  }}
+import 'package:flutter/material.dart';
+
+//FIXME:Material is used for adding material design to widget.Material is general Widget
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+        color: Colors.greenAccent,
+        borderRadius: BorderRadius.circular(70.0),
+        //center is very generic container
+
+        child: new Center(
+          child: new Text(
+            "Hello Everyone",
+            textDirection: TextDirection.ltr,
+            softWrap: true,
+            style: new TextStyle(
+              inherit: true,
+              color: Colors.deepPurple,
+              fontSize: 70.0,
+              fontStyle: FontStyle.italic,
+              letterSpacing: 2.0,
+              wordSpacing: 1.0,
+            ),
+            textAlign: TextAlign.center,
+            maxLines: 3,
+          ),
+        ));
+  }
+}
